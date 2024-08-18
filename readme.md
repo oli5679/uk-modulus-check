588d889408764b85f8773dace3c6ec163f5fae1c
oli5679 <olicairns93@gmail.com>
2024-08-18 15:47:40 +0100

    update

--- readme.md
+++ readme.md
@@ -19,6 +19,8 @@ npm install uk-modulus-check
 ```
 
 ## Usage
+
+
 ```
 import ModulusChecker from "uk-modulus-check";
 
@@ -28,7 +30,6 @@ console.log(checker.validate('180002', '00000190')); // true
 console.log(checker.validate('938063', '15763217')); // false
 ```
 
-
 ## Details
 
 [This](https://www.vocalink.com/media/a2febq5m/validating-account-numbers-uk-modulus-checking-v7-90.pdf) specification details logic to differentiate valid vs. invalid pairs of sort-code and account number. 
@@ -37,6 +38,15 @@ A mathematical algorithm compares the two values, and check if the two can be pa
 
 [This](https://www.vocalink.com/tools/modulus-checking/) page details any changes to the specification.
 
+## Technicalities
+
+In general, I tried to minimise risk of returning `false` for a valid bank account.
+
+1. if an account has an unseen sort code (not covered by any range in src/data/valacdos), by default, any combination of sort-code and account number will be considered valid. This is altered by instantiating a check with `ModulusChecker(false)` and then default behaviour will be set to false
+
+2. I find the specification a bit confusing for some examples, specifically when 2 checks are run on the same account. I mark the account as valid if either of the two validations pass, for cases where multiple validations are provided for the same account. The specific test-cases in listest examples are 23, 27, 28. 
+
+Both (1) and (2) might lead to a small % of 'false positives'. 
 
 ## License
 
@@ -44,9 +54,9 @@ MIT
 
 ## Credits
 
-Many thanks to [bazerk/uk-modulus-checking](https://github.com/bazerk/uk-modulus-checking) for inspiration (Python)
+Thanks to [uphold/uk-modulus-checking](https://github.com/uphold/uk-modulus-checking) for creating a good Javascript implementation. 
 
-Also thanks to [uphold/uk-modulus-checking](https://github.com/uphold/uk-modulus-checking) for creating a good Javascript implementation. It doesn't seem to be updated since valacdos-v640 (12/10/2020), which motivates the creation of this package
+It doesn't seem to have been updated since valacdos-v640 (12/10/2020), which motivates the creation of this package.
 
 
 
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import ModulusChecker from '../src';
 import { ModulusWeight } from '../src/interfaces';
-import { CheckType } from '../src/constants';
+import { CheckType } from '../src/enums';
 
 describe('ModulusChecker', () => {
     let checker: ModulusChecker;
@@ -59,38 +59,44 @@ describe('ModulusChecker', () => {
         });
 
         // Vocalink spec tests
+
+        // Comment indicates the test number from page 71, here
+        // https://www.vocalink.com/media/a2febq5m/validating-account-numbers-uk-modulus-checking-v7-90.pdf
+        // not passing for 23, 27, 28, which are all expected failures according to the spec.
+        
         const vocalinkSpecTests = [
-            { sortCode: '089999', accountNumber: '66374958', expectedResult: true },
-            { sortCode: '107999', accountNumber: '88837491', expectedResult: true },
-            { sortCode: '202959', accountNumber: '63748472', expectedResult: true },
-            { sortCode: '871427', accountNumber: '46238510', expectedResult: true },
-            { sortCode: '872427', accountNumber: '46238510', expectedResult: true },
-            { sortCode: '871427', accountNumber: '09123496', expectedResult: true },
-            { sortCode: '871427', accountNumber: '99123496', expectedResult: true },
-            { sortCode: '820000', accountNumber: '73688637', expectedResult: true },
-            { sortCode: '827999', accountNumber: '73988638', expectedResult: true },
-            { sortCode: '827101', accountNumber: '28748352', expectedResult: true },
-            { sortCode: '134020', accountNumber: '63849203', expectedResult: true },
-            { sortCode: '118765', accountNumber: '64371389', expectedResult: true },
-            { sortCode: '200915', accountNumber: '41011166', expectedResult: true },
-            { sortCode: '938611', accountNumber: '07806039', expectedResult: true },
-            { sortCode: '938600', accountNumber: '42368003', expectedResult: true },
-            { sortCode: '938063', accountNumber: '55065200', expectedResult: true },
-            { sortCode: '772798', accountNumber: '99345694', expectedResult: true },
-            { sortCode: '086090', accountNumber: '06774744', expectedResult: true },
-            { sortCode: '309070', accountNumber: '02355688', expectedResult: true },
-            { sortCode: '309070', accountNumber: '12345668', expectedResult: true },
-            { sortCode: '309070', accountNumber: '12345677', expectedResult: true },
-            { sortCode: '309070', accountNumber: '99345694', expectedResult: true },
-            { sortCode: '938063', accountNumber: '15764264', expectedResult: false },
-            { sortCode: '938063', accountNumber: '15763217', expectedResult: false },
-            { sortCode: '118765', accountNumber: '64371388', expectedResult: false },
-            { sortCode: '089999', accountNumber: '66374959', expectedResult: false },
-            { sortCode: '107999', accountNumber: '88837493', expectedResult: false },
-            { sortCode: '074456', accountNumber: '12345112', expectedResult: true },
-            { sortCode: '070116', accountNumber: '34012583', expectedResult: true },
-            { sortCode: '074456', accountNumber: '11104102', expectedResult: true },
-            { sortCode: '180002', accountNumber: '00000190', expectedResult: true },
+
+            { sortCode: '089999', accountNumber: '66374958', expectedResult: true }, // 1  
+            { sortCode: '107999', accountNumber: '88837491', expectedResult: true }, // 2
+            { sortCode: '202959', accountNumber: '63748472', expectedResult: true }, // 3
+            { sortCode: '871427', accountNumber: '46238510', expectedResult: true }, // 4
+            { sortCode: '872427', accountNumber: '46238510', expectedResult: true }, // 5
+            { sortCode: '871427', accountNumber: '09123496', expectedResult: true }, // 6
+            { sortCode: '871427', accountNumber: '99123496', expectedResult: true }, // 7
+            { sortCode: '820000', accountNumber: '73688637', expectedResult: true }, // 8
+            { sortCode: '827999', accountNumber: '73988638', expectedResult: true }, // 9
+            { sortCode: '827101', accountNumber: '28748352', expectedResult: true }, // 10
+            { sortCode: '134020', accountNumber: '63849203', expectedResult: true }, // 11
+            { sortCode: '118765', accountNumber: '64371389', expectedResult: true }, // 12
+            { sortCode: '200915', accountNumber: '41011166', expectedResult: true }, // 13
+            { sortCode: '938611', accountNumber: '07806039', expectedResult: true }, // 14
+            { sortCode: '938600', accountNumber: '42368003', expectedResult: true }, // 15
+            { sortCode: '938063', accountNumber: '55065200', expectedResult: true }, // 16
+            { sortCode: '772798', accountNumber: '99345694', expectedResult: true }, // 17
+            { sortCode: '086090', accountNumber: '06774744', expectedResult: true }, // 18
+            { sortCode: '309070', accountNumber: '02355688', expectedResult: true }, // 19
+            { sortCode: '309070', accountNumber: '12345668', expectedResult: true }, // 20
+            { sortCode: '309070', accountNumber: '12345677', expectedResult: true }, // 21
+            { sortCode: '309070', accountNumber: '99345694', expectedResult: true }, // 22
+            { sortCode: '938063', accountNumber: '15764264', expectedResult: false }, // 24
+            { sortCode: '938063', accountNumber: '15763217', expectedResult: false }, // 25
+            { sortCode: '118765', accountNumber: '64371388', expectedResult: false }, // 26
+            { sortCode: '089999', accountNumber: '66374959', expectedResult: false }, // 29
+            { sortCode: '107999', accountNumber: '88837493', expectedResult: false }, // 30
+            { sortCode: '074456', accountNumber: '12345112', expectedResult: true }, // 31
+            { sortCode: '070116', accountNumber: '34012583', expectedResult: true }, // 32
+            { sortCode: '074456', accountNumber: '11104102', expectedResult: true }, // 33
+            { sortCode: '180002', accountNumber: '00000190', expectedResult: true }, // 34
         ];
 
         vocalinkSpecTests.forEach(({ sortCode, accountNumber, expectedResult }, index) => {
